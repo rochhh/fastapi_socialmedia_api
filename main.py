249c1ebe0e5b4d5dd02493c126e6cf8e4a7392d6
Roch <rochishnu7@gmail.com>
2023-10-17 12:43:45 +0530

    CRUD impl in fastapi done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import randrange
 from typing import Optional
-from fastapi import FastAPI 
+from fastapi import FastAPI , Response , status , HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 
@@ -21,20 +21,95 @@ my_posts = [
 ]
 
 @app.get("/posts")
-async def root():
+async def get_posts():
     return {"data_to_fe": my_posts}
 
 
-@app.post("/posts")
-async def root( payload : Post  ):                           # get data from the FE
+@app.post("/posts" , status_code=status.HTTP_201_CREATED)
+async def create_posts( payload : Post  ):                           # get data from the FE
     
     post_dict = payload.model_dump()                         
     post_dict["id"] = randrange(0,1000000)
     
     my_posts.append(post_dict)
 
-
-
     return {"message": f" the req was sent to the BE ! , {post_dict}" }
 
+
+
+def find_post(id):
+
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+        
+
+
+@app.get("/posts/{id}")
+async def get_post(id : int , response : Response ):                             # id from url can be obtained as a param arg of def 
+                                                                                 # response : Response is for http status codes  
+    ''' 
+    the id : int is done to directly add the validation to check if the id is an int or not 
+    and tries to convert it to the int format as well ! 
+    '''
+
+                                                                                # the url id is a string and we have to convert it to an int for the find_post() to work 
+    
+    post = find_post(id)
+
+    if not post:
+        # response.status_code = status.HTTP_404_NOT_FOUND 
+        # return {"message": f" Id -> { id } does not exist "}
+
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND , 
+            detail= f" Id -> { id } does not exist "
+            )
+    
+    return {"message" : f"the post id is -> { post }"}
+
+
+
+def find_index_post(id):
+
+    for idx , post in enumerate(my_posts):
+
+        if post["id"] == id:
+            return idx
+        
+
+
+@app.delete("/posts/{id}" , status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id : int):
+     
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail=f"The id {id} does not exist ")
+    
+    my_posts.pop(index)
+
+    return Response( status_code=status.HTTP_204_NO_CONTENT )
+
+
+
+@app.put("/posts/{id}")
+def update_post(id : int , post : Post ):
+    
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail=f"The id {id} does not exist ") 
+    
+    # for idx , post in enumerate(my_posts):
+
+    #     if post["id"] == id:
+
+    #         post[idx] = post
+
+    post_dict = post.model_dump()
+    post_dict["id"] = id
+    my_posts[index] = post
+
+    return {"data": post_dict}
 
